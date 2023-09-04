3b3f5f8e977ab3c39b8bcb9191ee852a5b85ea7b
RYAN T SOUTHARD <eodchop@gmail.com>
2023-09-04 21:42:03 +0000

    slicing without positional index values

--- Python Masterclass/Strings/strings2.py
+++ Python Masterclass/Strings/strings2.py
@@ -4,7 +4,7 @@ parrot = "Norwegian Blue"
 print(parrot[0:6]) #Norweg
 
 print(parrot[3:5])
-print(parrot[0:0])
 print(parrot[:9])
-
-
+print(parrot[10:])
+print(parrot[:6] + parrot[6:])
+print(parrot[:])
