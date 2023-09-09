cd79f542fc3dc1d57162e8b0ba4581b54eb93477
RYAN T SOUTHARD <eodchop@gmail.com>
2023-09-09 13:26:30 +0000

    renamed files

---
+++ Python Masterclass/Strings/sliceBack.py
@@ -0,0 +1,10 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[10::-1]
+qpo = letters[16:13:-1]
+edcba = letters[4::-1]
+zyxwvuts = letters[25:17:-1]
+
+print(zyxwvuts)
+print(qpo)
+print(edcba)
