22ab1ef3b843d8bb894d104723433d26634a7937
RYAN T SOUTHARD <eodchop@gmail.com>
2023-09-09 13:43:11 +0000

    new lesson

---
+++ Python Masterclass/sequenceOperators.py
@@ -0,0 +1,14 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords"
+
+print(string1 + string2 + string3 + string4 + string5 )
+print("he's " "probably " "pining " "for the " "fjords")
+print("\nHello " * 5)
+print("hello " * (5+4))
+print("Hello " * 5 + "4")
+
+today = "Friday"
+print("friday" in today)
