227de03fd6cd873e41391cf0a12f5666c04a8c6a
RYAN T SOUTHARD <eodchop@gmail.com>
2023-09-09 20:57:23 +0000

    Initial Commit

---
+++ Python Masterclass/Strings/formatting.py
@@ -0,0 +1,8 @@
+for i in range(1,13):
+    print("Number {0:2} squared is {1:3} and cubed is {2:4}.".format(i, i ** 2, i ** 3))
+
+print()
+for i in range(1,13):
+    print("Number {0:2} squared is {1:<3} and cubed is {2:^4}.".format(i, i ** 2, i ** 3))
+
+print("Pi is approximately {0:12}".format(22 /7))
