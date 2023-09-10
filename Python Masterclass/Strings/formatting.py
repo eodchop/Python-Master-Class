9ad718905ef62ad1ad884e4c8569a41da9ea41eb
RYAN T SOUTHARD <eodchop@gmail.com>
2023-09-10 13:44:35 +0000

    More string formatting

--- Python Masterclass/Strings/formatting.py
+++ Python Masterclass/Strings/formatting.py
@@ -6,3 +6,6 @@ for i in range(1,13):
     print("Number {0:2} squared is {1:<3} and cubed is {2:^4}.".format(i, i ** 2, i ** 3))
 
 print(type("Pi is approximately {0:12}".format(22 /7)))
+
+
+
---
+++ Python Masterclass/Strings/fstrings.py
@@ -0,0 +1,18 @@
+greeting = "My name is "
+
+name = "Ryan"
+
+print(greeting + name)
+
+age = int(45)
+print(age)
+print(type(greeting))
+print(type(age))
+print(type(name))
+
+ageInWords = "forty four"
+print(type(ageInWords))
+print(ageInWords)
+print(name + f" is {age} years old.")
+
+print(f"Pi is approximately {22/7:12.50}")
