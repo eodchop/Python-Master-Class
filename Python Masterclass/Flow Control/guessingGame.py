cca07a62fe6a780c3ea43a8807c6d507167c3911
eodchop <eodchop@gmail.com>
2023-09-14 19:38:38 -0500

    different way to do the same thing.

--- Python Masterclass/Flow Control/guessingGame.py
+++ Python Masterclass/Flow Control/guessingGame.py
@@ -2,20 +2,38 @@ answer = 5
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Ha ha. You're a failure. Too Low.")
-    guess = int(input("Please guess again: "))
-    if guess == answer:
-        print("Well Done. You guessed it.")
+if guess != answer:
+    if guess < answer:
+        print("You guessed too low.")
+        guess = int(input())
     else:
-        print("You have not guessed correctly.")
-elif guess > answer:
-    print("Ha ha. You're a failure. You guessed too high.")
-    guess = int(input("Please guess again: "))
+        print("You guessed too high. Please guess lower.")
+        guess = int(input())
     if guess == answer:
         print("Well done. You chose wisely.")
     else:
-        print("You have not guessed correctly.")
+        print("Sorry you chose incorrectly.")
 else:
-    print("You guessed right")
+    print("You chose wisely!")
+
+
+
+# if guess < answer:
+#     print("Ha ha. You're a failure. Too Low.")
+#     guess = int(input("Please guess again: "))
+#     if guess == answer:
+#         print("Well Done. You guessed it.")
+#     else:
+#         print("You have not guessed correctly.")
+# elif guess > answer:
+#     print("Ha ha. You're a failure. You guessed too high.")
+#     guess = int(input("Please guess again: "))
+#     if guess == answer:
+#         print("Well done. You chose wisely.")
+#     else:
+#         print("You have not guessed correctly.")
+# else:
+#     print("You guessed right")
+
+
 
